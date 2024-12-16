351f0a921c66df9305e12759ad6af326180ccdcb
Raon98 <cheol1998@kakao.com>
2024-12-16 20:27:28 +0900

    feat : 변수 자료형 할당 이론실습

---
+++ src/CarTest.java
@@ -0,0 +1,20 @@
+public class CarTest {
+    public static void main(String[] args) {
+        String model="";
+        System.out.println("model = " + model);
+        long distance =1000000;
+        System.out.println("distance = " + distance + "KM");
+        int price = 9000000;
+        System.out.println("price = " + price);
+        String company = "BMW";
+        System.out.println("company = " + company);
+        char type ='A';
+        System.out.println("`type` = " + type);
+        boolean auto = true;
+        System.out.println("`auto` = " + auto);
+        int year =2000;
+        System.out.println("year` = " + year);
+        float gasmi = 12.5F;
+        System.out.println("gasmi = " + gasmi + "L");
+    }
+}

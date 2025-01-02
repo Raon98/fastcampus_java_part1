619c3ad5d8f501ec40b297008c5d8f37c709fb39
Raon98 <cheol1998@kakao.com>
2025-01-02 19:22:04 +0900

    feat : 객체를 생성하는 방법

---
+++ src/MemberTest.java
@@ -0,0 +1,10 @@
+public class MemberTest {
+    public static void main(String[] args) {
+        Member m = new Member();
+        m.name= "홍길동";
+        m.age= 20;
+        m.tel="010-1234-5678";
+        m.email="fc@naver.com";
+        m.weight=57.6f;
+    }
+}

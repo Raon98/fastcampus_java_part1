ce88ef4d310c1531d82d1e029982a621219425b8
Raon98 <cheol1998@kakao.com>
2025-01-02 19:14:38 +0900

    feat : 새로운 자료형 만들기

---
+++ src/BookTest.java
@@ -0,0 +1,17 @@
+public class BookTest {
+    public static void main(String[] args) {
+        // 정수 한 개를 저장할 변ㅅ후를 선언하시오
+        int a;
+        //  책한권을 저장할 변수를 선언핫히오
+
+        String title ="자바";
+        int price =30000;
+        String company ="패스트캠퍼스";
+        String author ="박매일";
+        int page =700;
+
+        Book b = new Book();
+
+    }
+
+}

e5a40cae800f1f868f8568abf87c758b5c2b857f
Raon98 <cheol1998@kakao.com>
2025-01-03 19:10:34 +0900

    feat : 배열을 사용하는 방법

---
+++ src/ArrayMake.java
@@ -0,0 +1,10 @@
+public class ArrayMake {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+
+        for(int i=0; i<a.length; i++) {
+            a[i] = i+1;
+        }
+
+    }
+}

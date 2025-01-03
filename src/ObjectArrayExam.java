24ce9b50888a758e513c7272bb203c304980b828
Raon98 <cheol1998@kakao.com>
2025-01-03 19:23:57 +0900

    feat : 기본배열과 객체배열2

---
+++ src/ObjectArrayExam.java
@@ -0,0 +1,9 @@
+public class ObjectArrayExam {
+    public static void main(String[] args) {
+        Movie[] m = new Movie[3];
+
+        m[0] = new Movie();
+        m[0].title = "spiderman";
+        m[0].director = "spiderman";
+    }
+}

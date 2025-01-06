a4749834faa38be3c7a4be17f489edf50a6510e5
Raon98 <cheol1998@kakao.com>
2025-01-06 19:21:29 +0900

    feat : 연산자와 명령어를 이해하라

---
+++ src/ScannerTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("정수를 입력하세요 : ");
+        int num = scan.nextInt(); //블럭상태
+        System.out.println("num = " + num);
+
+        System.out.print("싨후를 입력하세요 : ");
+        float f= scan.nextFloat();
+        System.out.println("f = " + f);
+
+        System.out.print("문자열 입력하세요 : ");
+        String str = scan.next();
+        System.out.println("str = " + str);
+
+        scan.nextLine(); //버퍼비우기 (스트림 비욱리)
+    }
+}
